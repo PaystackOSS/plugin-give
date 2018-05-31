af8f894c516bb745d410df04f69c1c88ae0b053a
Stephen Amaza <steveamaza@gmail.com>
2018-05-31 14:31:54 +0100

    Initial commit
    Use http://wppb.me/ boilerplate to generate skeletal structure

---
+++ admin/partials/paystack-give-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://paystack.com
+ * @since      1.0.0
+ *
+ * @package    Paystack_Give
+ * @subpackage Paystack_Give/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
