e083f47a2136603213c54deb3c1859e8b5fb7b6b
lukman-paystack <58217917+lukman-paystack@users.noreply.github.com>
2021-01-04 09:41:46 +0100

    update version number

--- paystack-give.php
+++ paystack-give.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Paystack Payments for Give
  * Plugin URI:        http://wordpress.org/plugins/paystack-give
  * Description:       Paystack integration for accepting payments via card, bank accounts, USSD and mobile money
- * Version:           1.0.0
+ * Version:           1.2.0
  * Author:            Paystack
  * Author URI:        https://paystack.com
  * License:           GPL-2.0+
